27e32e966ee70223329f344bda32bc1888343392
JayanthThoppil <48017821+JayanthThoppil@users.noreply.github.com>
2021-03-16 00:15:43 +0530

    connected db to mongodb atlas

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,11 +1,22 @@
 const mongoose = require('mongoose');
+const DB ='mongodb+srv://jay:qwert@cluster0.xyfk4.mongodb.net/crudap?retryWrites=true&w=majority';
 
-mongoose.connect("mongodb://localhost:27017/students-api",{
+mongoose.connect(DB,{
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true 
+    useUnifiedTopology: true,
+    useFindAndModify:false 
 }).then(() => {
     console.log("connection successfull..");
-}).catch((e) => {
-    console.log("No connection");
+}).catch((error) => {
+    console.log(error);
 })
+// mongoose.connect("mongodb://localhost:27017/students-api",{
+//     useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true 
+// }).then(() => {
+//     console.log("connection successfull..");
+// }).catch((e) => {
+//     console.log("No connection");
+// })
